df8125094000be3ac7ddc1913580f2def68ce778
KohCharles <charles@creatual.io>
2026-07-02 17:51:52 +0800

    amplifyconfig moved

--- app/components/AmplifyProvider.tsx
+++ app/components/AmplifyProvider.tsx
@@ -1,13 +1,10 @@
 "use client";
 
-import { useEffect } from "react";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 
-export function AmplifyProvider({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    Amplify.configure(outputs, { ssr: true });
-  }, []);
+Amplify.configure(outputs, { ssr: true });
 
+export function AmplifyProvider({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
 }
